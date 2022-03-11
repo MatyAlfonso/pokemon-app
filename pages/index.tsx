52ee425ad737da0a2a86b0a08c1e84c744059c1e
MatyAlfonso <matt1998boca@gmail.com>
2022-03-11 21:35:14 +0100

    Update Layout title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
-    <Layout title='Listado de Pokémon'>
+    <Layout title='Pokémon List'>
       <PokemonList pokemons={pokemons} />
     </Layout>
   );
